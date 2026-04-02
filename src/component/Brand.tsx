bdcfbd2d980eee6bc06bbd5e0ca8d7217da0b98d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-04-03 01:13:28 +0530

    feat(component): add brand component

---
+++ src/component/Brand.tsx
@@ -0,0 +1,8 @@
+
+const Brand = () => {
+  return (
+    <div><h1 id="brand" className="leading-[2.85rem] hover:cursor-pointer" style={{fontSize:"var(--font-size-big)"}}>Elementum</h1></div>
+  )
+}
+
+export default Brand
