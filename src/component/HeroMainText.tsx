d76b7f21ac4b87c50e503984fb16a42d2ff398dd
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2026-04-03 23:35:10 +0530

    feat(component): add text component for hero section

---
+++ src/component/HeroMainText.tsx
@@ -0,0 +1,17 @@
+
+const HeroMainText = () => {
+  return (
+    <div>
+        <h1 id="brand" className="text-center leading-31.5 tracking-[0px]" style={{fontSize:"var(--font-size-title)"}}>
+            The <span className="relative">thinkers
+            <svg width="595" height="56" viewBox="0 0 595 56" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute left-1/2 -translate-1/2 top-full scale-70">
+            <path d="M72.1335 3H538.634L0.133545 27H594.134L293.134 53" stroke="#FFC250" stroke-width="6"/>
+            </svg>
+            </span> and
+            <br />doers were <span className="relative">changing<div className="absolute w-4/5 h-full top-3 -right-10 rounded-full bg-[#FFC2EA] -z-10"></div></span> <br />
+            the <span className="relative">status<div className="absolute w-full h-full bg-[#D7EEDD] top-0 left-0 -z-10 scale-x-125 rounded-full"></div></span> Quo with</h1>
+    </div>
+  )
+}
+
+export default HeroMainText
